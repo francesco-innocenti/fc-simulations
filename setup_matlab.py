7303ea534c79136c0eaf5f4c907099fd2f95f9a0
Francesco Innocenti <F.Innocenti@sussex.ac.uk>
2021-11-08 18:21:47 +0000

    refactor matlab in `setup_matlab`

---
+++ setup_matlab.py
@@ -0,0 +1,10 @@
+import matlab.engine
+
+def setup_matlab():
+
+    eng = matlab.engine.start_matlab()
+
+    # add MVGC2 paths
+    eng.addpath("/Users/FrancescoInnocenti/Documents/MATLAB/MVGC2/core")
+    eng.addpath("/Users/FrancescoInnocenti/Documents/MATLAB/MVGC2/utils")
+    eng.addpath("/Users/FrancescoInnocenti/Documents/MATLAB/MVGC2/demo")
