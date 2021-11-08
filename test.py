fe91e08c0ea9058a03153e91ba9f1c2cd41ca1a0
Francesco Innocenti <F.Innocenti@sussex.ac.uk>
2021-11-08 11:44:08 +0000

    fix Python-Matlab data type conversion

--- test.py
+++ test.py
@@ -4,6 +4,25 @@ eng = matlab.engine.start_matlab()
 
 # add MVGC2 path
 eng.addpath('/Users/FrancescoInnocenti/Documents/MATLAB/MVGC2/demo')
+eng.addpath('/Users/FrancescoInnocenti/Documents/MATLAB/MVGC2/utils')
 
-data = eng.tnet5()
-print(data)
+# test data generation
+n_trials = float(4)
+n_obs = float(500)
+fs = float(200)
+
+# actual (ground truth) VAR model generation parameters
+tnet = eng.tnet5()
+moact = float(6)
+rho = 0.95
+wvar = 0.5
+rmi = 0.5
+
+# generate random VAR test data
+
+# set RNG
+seed = float(123)
+eng.rng(seed)
+
+# generate random VAR coefficients for test network
+AA = eng.var_rand(tnet, moact, rho, wvar)
