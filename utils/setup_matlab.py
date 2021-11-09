ad66c64324ee49ff98447b12ab7323b22de56b9b
Francesco Innocenti <F.Innocenti@sussex.ac.uk>
2021-11-09 10:07:53 +0000

    add path to MVGC2 stats folder for access to `demean`

--- utils/setup_matlab.py
+++ utils/setup_matlab.py
@@ -7,6 +7,7 @@ def setup_matlab():
     # add MVGC2 paths
     eng.addpath("/Users/FrancescoInnocenti/Documents/MATLAB/MVGC2/core")
     eng.addpath("/Users/FrancescoInnocenti/Documents/MATLAB/MVGC2/utils")
+    eng.addpath("/Users/FrancescoInnocenti/Documents/MATLAB/MVGC2/stats")
     eng.addpath("/Users/FrancescoInnocenti/Documents/MATLAB/MVGC2/demo")
 
     return eng
